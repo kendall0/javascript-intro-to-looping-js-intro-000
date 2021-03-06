d6b57f16d6fa992e908e07784f8a35d6f4ed6d5e
Kendall George <kendallanngeorge@gmail.com>
2021-03-06 19:45:15 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,29 @@
+function forLoop(array) {
+  const array = []
+  const t = 25
+for (let i = 0; i < t; i++) {
+  if (i === 1) {
+    array.push('I am 1 strange loop.');
+  } else {
+    array.push(I am ${i} strange loops.);
+  }
+    }
+    return [array, i]
+}
+const [array, t] = makeArray()
+const strangeArray = forLoop(array)
+const testArray = strangeArray.slice(array.length)
+
+function whileLoop(n) {
+ // while (n > 0){
+  //  console.log(--n)
+ // }
+ // return "done";
+}
+
+function doWhileLoop(num){
+//  {
+ //   console.log("I run once regardless.");
+ // } while (num === 0);
+  // num += 1
+}
